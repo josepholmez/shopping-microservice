58416fab1f61089792b95d50663074841a9bc8a9
JosephPersonal <joe.olmez@gmail.com>
2023-06-25 18:21:01 -0400

    added discovery server using eureka

--- microservices-parent/product-service/src/main/java/com/olmez/productservice/ProductServiceApplication.java
+++ microservices-parent/product-service/src/main/java/com/olmez/productservice/ProductServiceApplication.java
@@ -5,11 +5,13 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
 
 import com.olmez.productservice.utility.SourceUtils;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class ProductServiceApplication {
 
 	private Logger log = LoggerFactory.getLogger(getClass());
